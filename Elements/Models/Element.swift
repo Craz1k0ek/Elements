ea21d2b101f1073bb68be50144500430ed4e26bf
Bram <kolkmanbram@hotmail.com>
2020-12-06 20:02:45 +0100

    Resource loading
    * Implemented a static all for all the elements
    * Fixed some of the types to match the data source

--- Elements/Models/Element.swift
+++ Elements/Models/Element.swift
@@ -2,16 +2,26 @@ import Foundation
 
 // MARK: Element
 
-struct Element: Hashable {
+struct Element: Decodable, Hashable {
+    /// All elements from the loaded resource file.
+    static var all: [Element] = {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        // All these bang operators are safe, as the data is validated and MUST
+        // be included in a running app.
+        return try! decoder.decode([Element].self, from: try! Data(contentsOf: Bundle.main.url(forResource: "elements", withExtension: "json")!))
+    }()
+    
     let number: Int
     let symbol: String
     let name: String
     
     let atomicMass: Float
-    let density: Float
+    let density: Float?
     
-    let boil: Float
-    let melt: Float
+    let boil: Float?
+    let melt: Float?
     let phase: Phase
     
     let discoveredBy: String?
@@ -23,7 +33,7 @@ struct Element: Hashable {
 // MARK: Element Phase
 
 extension Element {
-    enum Phase: Int, CustomStringConvertible {
+    enum Phase: Int, CustomStringConvertible, Decodable {
         case gas, solid, liquid
         
         var description: String {
